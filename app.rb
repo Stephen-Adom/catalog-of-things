841da2490f97b44eca2999d48751f820e0958403
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-05-29 13:36:56 +0000

    fix linters

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ class App
 
   def app_options
     puts 'Please choose an option by entering a number:'
-    puts "1 - List all books\n2 - List all music albums\n3 - List all movies\n4 - List all games\n5 - List all genres (e.g Comedy, Thriller)\n6 - List all labels (eg Gift, New)\n7 - List all authors (eg Stephen King)\n8 - List all sources (eg From a friend, Online shop)\n9 - Add a book\n10 - Add a music album\n11 - Add a movie\n12 - Add a game\n13 - Exit"
+    puts "1 - List all books\n2 - List all music albums\n3 - List all movies\n4 - List all games\n5 - List all genres
+    (e.g Comedy, Thriller)\n6 - List all labels (eg Gift, New)\n7 - List all authors (eg Stephen King)\n8 -
+    List all sources (eg From a friend, Online shop)\n9 - Add a book\n10 - Add a music album\n11 -
+    Add a movie\n12 - Add a game\n13 - Exit"
     print 'Your option '
     @option = gets.chomp
   end
