0e84582f6140a924f0f93432ea36d222a1ce7934
veronica365 <naruth10@gmail.com>
2023-05-30 22:35:49 +0300

    fix lint errors

--- classes/genre_app.rb
+++ classes/genre_app.rb
@@ -25,7 +25,7 @@ class GenreApp
 
     def list_all
       puts "Loading all genres ... \n"
-      return puts 'No music genres found' unless !@genres.empty?
+      return puts 'No music genres found' if @genres.empty?
 
       genre_data = Storage.read_file_content(@file_name)
       genre_data.each_with_index do |genre, index|
