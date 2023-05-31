5c005c5f74af9392f6fd259ce4e94d5ec4e7d582
veronica365 <naruth10@gmail.com>
2023-05-31 15:51:00 +0300

    Fix failing tests for the music album

--- classes/music.rb
+++ classes/music.rb
@@ -4,7 +4,7 @@ class MusicAlbum < Item
   attr_accessor :on_spotify
 
   def initialize(on_spotify, publish_date)
-    super(publish_date)
+    super(genre, author, label, publish_date)
     @on_spotify = on_spotify
   end
 
