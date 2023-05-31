bae1bc7c50492512b361089866a559d7be547d16
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-05-31 14:44:59 +0200

    Fix rspec error

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,20 +1,18 @@
 require_relative '../classes/author'
-
+require_relative '../classes/item'
 
 RSpec.describe Author do
   let(:author) { Author.new('John', 'Doe') }
 
   describe '#add_item' do
     it 'adds an item to the author' do
-      item = double('Item')
-      expect(item).to receive(:author=).with(author)
+      item = Item.new('Genre', author, 'Label', Date.today)
       author.add_item(item)
       expect(author.items).to include(item)
     end
 
     it 'does not add the same item twice' do
-      item = double('Item')
-      expect(item).to receive(:author=).with(author).once
+      item = Item.new('Genre', author, 'Label', Date.today)
       author.add_item(item)
       author.add_item(item)
       expect(author.items.size).to eq(1)
