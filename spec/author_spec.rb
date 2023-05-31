a3a15208ba6db9885d53e3688f0035117ce47a6c
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-05-31 14:36:50 +0200

    Add rspec tests

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,1 +1,24 @@
 require_relative '../classes/author'
+
+require_relative '../classes/author'
+
+RSpec.describe Author do
+  let(:author) { Author.new('John', 'Doe') }
+
+  describe '#add_item' do
+    it 'adds an item to the author' do
+      item = double('Item')
+      expect(item).to receive(:author=).with(author)
+      author.add_item(item)
+      expect(author.items).to include(item)
+    end
+
+    it 'does not add the same item twice' do
+      item = double('Item')
+      expect(item).to receive(:author=).with(author).once
+      author.add_item(item)
+      author.add_item(item)
+      expect(author.items.size).to eq(1)
+    end
+  end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,24 @@
 require_relative '../classes/game'
+
+require_relative '../classes/game'
+require_relative '../classes/author'
+
+RSpec.describe Game do
+  let(:author) { Author.new('John', 'Doe') }
+  let(:publish_date) { Date.new(2020, 1, 1) }
+  let(:game) { Game.new('Action', author, 'Game Label', publish_date) }
+
+  describe '#can_be_archived?' do
+    it 'returns true if the game can be archived' do
+      allow(Date).to receive(:today).and_return(Date.new(2032, 1, 1))
+      allow(game).to receive(:last_played_more_than_two_years_ago?).and_return(true)
+      expect(game.can_be_archived?).to be true
+    end
+
+    it 'returns false if the game cannot be archived' do
+      allow(Date).to receive(:today).and_return(Date.new(2025, 1, 1))
+      allow(game).to receive(:last_played_more_than_two_years_ago?).and_return(false)
+      expect(game.can_be_archived?).to be false
+    end
+  end
+end
